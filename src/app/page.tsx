7cb7f24e607cd7563d380ee44670289998c1749c
hilminur191 <hilminurbani98@gmail.com>
2025-07-16 12:44:58 +0700

    update 16 juli 25

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
 import Experience from "./components/Experience";
 import Testimonials from "./components/Testimonials";
